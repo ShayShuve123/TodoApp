48ceddb7e667766a81d13a22ac887857c2685448
shay.shuve <shayshov@gmail.com>
2026-02-09 00:33:56 +0200

    Update database.py

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,1 +1,5 @@
-SQLALCHEMY_DATABASE_URL =
+from sqlalchemy import create_engine
+
+SQLALCHEMY_DATABASE_URL = 'sql:///./todos.db' # used to create a location of this db on our fastapi application
+
+create_engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}) # db agent to Opan up a connction to use our db
